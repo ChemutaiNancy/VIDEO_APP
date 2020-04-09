1391610cd5f5c5ebfc4445ef8b7809a9bf8312d1
Nancy <chemutain@gmail.com>
2020-04-09 20:26:03 +0300

    Adding video to an app

---
+++ app/src/main/java/com/example/videoapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.videoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final VideoView myvideoview = findViewById(R.id.videoView);
+        //set link of the video
+        myvideoview.setVideoPath("https://www.youtube.com/watch?v=SWYqp7iY_Tc");
+
+        //make video start
+        myvideoview.start();
+    }
+}
